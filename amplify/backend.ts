48f08568364da98dcdd73b1729cee987308740d1
NGFW <rohanrob1@gmail.com>
2025-02-15 13:02:28 -0500

    s3-backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,8 +3,15 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: "us-east-1",
+    bucket_name: "mys3customapp"
+  },
 });
