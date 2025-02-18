f13d439a0ed7f1d7c0356dae119d5e083cd332e3
NGFW <rohanrob1@gmail.com>
2025-02-18 13:40:40 -0500

    files/

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
   access: (allow) => ({
-    'files/{entity_id}/*': [
+    'files/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ],
     'submissions/*': [
