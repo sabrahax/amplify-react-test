0f71cca120b19f491fd2d342df8b5646c99e338b
Sam Abraham <epicblubber@gmail.com>
2025-05-08 19:28:20 -0400

    please work

--- amplify-backup/storage/resource.ts
+++ amplify-backup/storage/resource.ts
@@ -3,7 +3,10 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
   access: (allow) => ({
-    '*': [
+    '/*': [
+      allow.authenticated.to(['read', 'write', 'delete'])
+    ],
+    'protected/*': [
       allow.authenticated.to(['read', 'write', 'delete'])
     ],
     'submissions/*': [
