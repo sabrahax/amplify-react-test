df8491899e57bcf7f635c347f80fb43ad7a86027
Sam Abraham <epicblubber@gmail.com>
2025-05-08 19:23:03 -0400

    Add amplify.yml and fix storage configuration

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,11 +3,8 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
   access: (allow) => ({
-    '/*': [
-      allow.authenticated.to(['read', 'write', 'delete'])
-    ],
-    'protected/*': [
-      allow.authenticated.to(['read', 'write', 'delete'])
+    'files/${cognito-identity.amazonaws.com:sub}/*': [
+      allow.identityBased.to(['read', 'write', 'delete'])
     ],
     'submissions/*': [
       allow.authenticated.to(['read', 'write'])
